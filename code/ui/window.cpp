fb4c83cad99e1b31a1d68bb7e00f21879384ea0c
TAK2004 <thomas@ptscientists.com>
2016-09-27 10:35:17 +0200

    -added opengl renderer

--- code/ui/window.cpp
+++ code/ui/window.cpp
@@ -2,14 +2,71 @@
 #include <RadonFramework/Radon.hpp>
 #include <RadonFramework/System/Environment.hpp>
 #include "window.hpp"
+#include <RadonFramework/System/Drawing/OpenGL.hpp>
 
 namespace RadonExample {
+
+
+void OpenGLRenderer::SimpleGLShader::Generate()
+{
+    const char* vertex_shader =
+        "#version 420\n"
+        "layout(location = 0) in vec3 vertex_position;"
+        "layout(location = 1) in vec3 vertex_color;"
+        "layout(std140, binding = 2) uniform SharedTransformUniforms"
+        "{"
+        "    mat4 ModelView;"
+        "    mat4 ModelViewProjection;"
+        "};"
+        "void main () {"
+        "  gl_Position = ModelViewProjection* vec4(vp, 1.0);"
+        "}";
+
+    const char* fragment_shader =
+        "#version 420\n"
+        "out vec4 frag_colour;"
+        "void main () {"
+        "  frag_colour = vec4 (0.5, 0.5, 0.5, 1.0);"
+        "}";
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    shader_programme = glCreateProgram();
+    glAttachShader(shader_programme, fs);
+    glAttachShader(shader_programme, vs);
+    glLinkProgram(shader_programme);
+
+    glCreateBuffers(1, &m_SharedUBO);
+    glNamedBufferData(m_SharedUBO, sizeof(SharedTransformUniforms), &m_SharedTransformUniforms, GL_STREAM_DRAW);
+}
+
+void OpenGLRenderer::SimpleGLShader::Bind()
+{
+    glUseProgram(shader_programme);
+}
+
+void OpenGLRenderer::SimpleGLShader::Unbind()
+{
+    glUseProgram(0);
+}
+
+void OpenGLRenderer::SimpleGLShader::Update()
+{
+    glBindBufferBase(GL_UNIFORM_BUFFER, 2, m_SharedUBO);
+    glNamedBufferSubData(m_SharedUBO, 0, sizeof(SharedTransformUniforms), &m_SharedTransformUniforms);
+}
 
 BasicWindow::BasicWindow()
 :m_Canvas(*this)
 ,m_Label1(&m_Canvas)
 {
     m_Label1.SetText(":)"_rfs);
+    m_OpenGLRenderer.SetCanvas(m_Canvas);
     m_Canvas.SetRenderer(m_OpenGLRenderer);
 }
 
@@ -20,22 +77,64 @@ RF_Form::Label& BasicWindow::GetLabel1()
 
 void OpenGLRenderer::StartFrame()
 {
-    
+    m_Canvas->Clear();
+    m_BasicShader.Bind();
+    m_BasicShader.m_SharedTransformUniforms.ModelView = m_Camera.GetMatrix();
+    m_BasicShader.m_SharedTransformUniforms.ModelViewProjection = m_Projection.GetMatrix(RF_Geo::Viewtype::View2D);
+    m_BasicShader.Update();
 }
 
 void OpenGLRenderer::EndFrame()
 {
-    
+
+    m_Canvas->SwapBuffer();
 }
 
 void OpenGLRenderer::Process(const RF_Draw::Path2D& Path)
-{
-    
+{    
+    auto hash = Path.GetHash();
+    void* data;
+    if(!m_Objects.Get(hash, data))
+    {
+        Object obj;
+        Path.Visit(m_Tesselator);
+        auto vertices = m_Tesselator.GetVertices();
+        auto colors = m_Tesselator.GetVertexColors();
+
+        glGenBuffers(2, obj.vbos);
+        glBindBuffer(GL_ARRAY_BUFFER, obj.vbos[0]);
+        glBufferData(GL_ARRAY_BUFFER, vertices.Count() * sizeof(RF_Geo::Vec3f), &vertices(0), GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, obj.vbos[1]);
+        glBufferData(GL_ARRAY_BUFFER, colors.Count() * sizeof(RF_Geo::Vec3f), &colors(0), GL_STATIC_DRAW);
+
+        glGenVertexArrays(1, &obj.vao);
+        glBindVertexArray(obj.vao);
+        glBindBuffer(GL_ARRAY_BUFFER, obj.vbos[0]);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glBindBuffer(GL_ARRAY_BUFFER, obj.vbos[1]);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        obj.vertices = vertices.Count();
+        m_ObjectPool.Resize(m_ObjectPool.Count()+1);
+        m_ObjectPool(m_ObjectPool.Count()-1) = obj;
+        m_Objects.Add(hash, &m_ObjectPool(m_ObjectPool.Count() - 1));
+    }
 }
 
 void OpenGLRenderer::Draw(const RF_Draw::Path2D& Path)
 {
-    
+    for (RF_Type::Size i = 0; i < m_ObjectPool.Count(); ++i)
+    {
+        glBindVertexArray(m_ObjectPool(i).vao);
+        glDrawArrays(GL_TRIANGLES, 0, m_ObjectPool(i).vertices);
+    }
+}
+
+void OpenGLRenderer::SetCanvas(RF_Draw::Canvas3D& Canvas)
+{
+    m_Canvas = &Canvas;
+    m_BasicShader.Generate();
 }
 
 }
--- code/ui/window.hpp
+++ code/ui/window.hpp
@@ -2,6 +2,7 @@
 #define RE_WINDOW_HPP
 
 #include <RadonFramework/Drawing/AbstractRenderer.hpp>
+#include <RadonFramework/Drawing/Path2DTriangulation.hpp>
 
 namespace RadonExample {
 
@@ -12,6 +13,39 @@ public:
     virtual void EndFrame() override;
     virtual void Process(const RF_Draw::Path2D& Path) override;
     virtual void Draw(const RF_Draw::Path2D& Path) override;
+    void SetCanvas(RF_Draw::Canvas3D& Canvas);
+private:
+    RF_Draw::Canvas3D* m_Canvas;
+    RF_Draw::Path2DTriangulation m_Tesselator;
+
+    struct Object
+    {
+        RF_Type::UInt32 vbos[2];
+        RF_Type::UInt32 vao, vertices;
+    };
+
+    class SimpleGLShader
+    {
+    public:
+        void Generate();
+        void Bind();
+        void Unbind();
+        void Update();
+        struct SharedTransformUniforms
+        {
+            RF_Geo::Mat4f ModelView;
+            RF_Geo::Mat4f ModelViewProjection;
+        } m_SharedTransformUniforms;
+    private:
+        RF_Type::UInt32 shader_programme = 0;
+        RF_Type::UInt32 m_SharedUBO = 0;
+    };
+
+    RF_Collect::Array<Object> m_ObjectPool;
+    RF_Collect::HashList m_Objects;
+    SimpleGLShader m_BasicShader;
+    RF_Draw::Camera m_Camera;
+    RF_Geo::ProjectionMatrix m_Projection;
 };
 
 class BasicWindow: public RF_Form::Form
